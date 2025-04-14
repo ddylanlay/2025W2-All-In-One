b88e2c39be051e1c0d32fcfbd40a71bbad41083b
Shen-SLJ <shen.jng1@gmail.com>
2025-04-14 23:51:03 +1000

    change run unit test command to 'npm run unitTest'

--- ui-modules/home-example/tests/home-page-slice.test.ts
+++ ui-modules/home-example/tests/home-page-slice.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@jest/globals";
 import reducer from "/ui-modules/home-example/state/reducers/home-page-slice";
 
-test("sum test", () => {
+test("when reducer is loaded, should return correct initial state", () => {
   expect(reducer(undefined, { type: 'unknown' })).toEqual({
     isLoading: true,
     taskDescriptions: [],
