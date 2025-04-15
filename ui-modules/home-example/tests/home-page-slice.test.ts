544bcc81a8efe7852b0245398098dfc73fb4528f
Shen-SLJ <shen.jng1@gmail.com>
2025-04-15 21:49:56 +1000

    add example tests for for the home page slice

--- ui-modules/home-example/tests/home-page-slice.test.ts
+++ ui-modules/home-example/tests/home-page-slice.test.ts
@@ -1,11 +1,56 @@
 import { expect, test } from "@jest/globals";
-import reducer from "/ui-modules/home-example/state/reducers/home-page-slice";
+import reducer, { loadTasks, updateTextboxValue } from "/ui-modules/home-example/state/reducers/home-page-slice";
+import { Task } from "/library-modules/domain-models/task-example/Task";
 
-test("when reducer is loaded, should return correct initial state", () => {
-  expect(reducer(undefined, { type: 'unknown' })).toEqual({
-    isLoading: true,
-    taskDescriptions: [],
-    taskIds: [],
-    exampleTextboxValue: "",
-  })
-})
+const initialState = {
+  isLoading: true,
+  taskDescriptions: [],
+  taskIds: [],
+  exampleTextboxValue: "",
+};
+
+test("when the reducer is loaded, then it should return correct initial state", () => {
+  // ACT
+  const res = reducer(undefined, { type: "unknown" });
+
+  // ASSERT
+  expect(res).toEqual(initialState);
+});
+
+test("when an update to the textbox value is sent, then the updated textbox value should match", () => {
+  // ARRANGE
+  const newTextboxValue = "test";
+  const expectedUpdatedState = {
+    ...initialState,
+    exampleTextboxValue: newTextboxValue,
+  }
+
+  // ACT
+  const updatedState = reducer(initialState, updateTextboxValue(newTextboxValue));
+
+  // ASSERT
+  expect(updatedState).toEqual(expectedUpdatedState);
+});
+
+test("when the tasks are fetched, then the state should be updated with task descriptions and ids that should match the same order", async () => {
+  // ARRANGE
+  const fakeTasks: Task[] = [
+    {
+      id: "afwn7JnD8",
+      text: "todo1",
+    },
+    {
+      id: "cFDd24dz",
+      text: "todo2",
+    },
+  ];
+  const expectedTaskIds = fakeTasks.map((task) => task.id)
+  const expectedTaskDescriptions = fakeTasks.map((task) => task.text)
+
+  // ACT
+  const updatedState = reducer(initialState, loadTasks.fulfilled(fakeTasks, ""));
+
+  // ASSERT
+  expect(updatedState.taskIds).toEqual(expectedTaskIds)
+  expect(updatedState.taskDescriptions).toEqual(expectedTaskDescriptions)
+});
