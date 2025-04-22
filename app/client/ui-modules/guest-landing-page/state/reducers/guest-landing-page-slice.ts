5c87c5bb2bbea9f0c3e93e3a72cd5d48af6cf186
shannonWallis <swal0059@student.monash.edu>
2025-04-22 23:19:14 +1000

    86cymwjqt - Create side, top, bottom nav and merge to main

---
+++ app/client/ui-modules/guest-landing-page/state/reducers/guest-landing-page-slice.ts
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { GuestLandingPageUiState } from "../GuestLandingPageUiState";
+import { RootState } from "/app/client/store";
+
+const initialState: GuestLandingPageUiState = {
+  isLoading: false,
+};
+
+export const guestLandingPageSlice = createSlice({
+  name: "guestLandingPage",
+  initialState: initialState,
+  reducers: {},
+});
+
+export const selectGuestLandingPageUiState = (state: RootState) =>
+  state.guestLandingPage;
+
+export default guestLandingPageSlice.reducer;
