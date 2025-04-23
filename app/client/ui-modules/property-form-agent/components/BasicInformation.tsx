8c21f3c2965ecd4bde0a67bf8e1de1a4d4c0cd3f
nalydh <dylanhoang9@gmail.com>
2025-04-23 15:41:30 +1000

    86cym3x36: Add Monthly Rent and Bond inputs

--- app/client/ui-modules/property-form-agent/components/BasicInformation.tsx
+++ app/client/ui-modules/property-form-agent/components/BasicInformation.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 import React from "react";
 import * as z from "zod";
-import {useForm} from "react-hook-form";
-import {zodResolver} from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
@@ -16,10 +16,9 @@ import {
   SelectContent,
   SelectItem,
   SelectTrigger,
-  SelectValue
+  SelectValue,
 } from "../../theming/components/shadcn/Select";
-import { Input}  from "../../theming/components/shadcn/Input";
-
+import { Input } from "../../theming/components/shadcn/Input";
 
 const basicInformationSchema = z.object({
   landlord: z.string(),
@@ -30,190 +29,192 @@ const basicInformationSchema = z.object({
   postal_code: z.string(),
   apartment_number: z.string().optional(),
   monthly_rent: z.string(),
-  bond: z.string()
+  bond: z.string(),
 });
 
 export default function BasicInformation() {
-
-const form = useForm < z.infer < typeof basicInformationSchema >> ({
-  resolver: zodResolver(basicInformationSchema),
-
-})
+  const form = useForm<z.infer<typeof basicInformationSchema>>({
+    resolver: zodResolver(basicInformationSchema),
+  });
 
   return (
     <div>
-<Form {...form}>
-<form className="space-y-8 max-w-3xl mx-auto py-10">
-  
-  <FormField
-    control={form.control}
-    name="landlord"
-    render={({ field }) => (
-      <FormItem>
-        <FormLabel>Select Landlord</FormLabel>
-        <Select onValueChange={field.onChange} defaultValue={field.value}>
-          <FormControl>
-            <SelectTrigger>
-              <SelectValue placeholder="Select a landlord" />
-            </SelectTrigger>
-          </FormControl>
-          <SelectContent className="bg-white">
-            <SelectItem value="Dylan Hoang">Dylan Hoang</SelectItem>
-            <SelectItem value="Marcus Bontempelli">Marcus Bontempelli</SelectItem>
-            <SelectItem value="Nick Daicos">Nick Daicos</SelectItem>
-          </SelectContent>
-        </Select>
-        <FormMessage />
-      </FormItem>
-    )}
-  />
+      <Form {...form}>
+        <form className="space-y-8 max-w-3xl mx-auto py-10">
+          <FormField
+            control={form.control}
+            name="landlord"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Select Landlord</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a landlord" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent className="bg-white">
+                    <SelectItem value="Dylan Hoang">Dylan Hoang</SelectItem>
+                    <SelectItem value="Marcus Bontempelli">
+                      Marcus Bontempelli
+                    </SelectItem>
+                    <SelectItem value="Nick Daicos">Nick Daicos</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-<FormField
-control={form.control}
-name="property_type"
-render={({ field }) => (
-  <FormItem>
-    <FormLabel>Property Type</FormLabel>
-    <Select onValueChange={field.onChange} defaultValue={field.value}>
-      <FormControl>
-        <SelectTrigger>
-          <SelectValue placeholder="Select property type" />
-        </SelectTrigger>
-      </FormControl>
-      <SelectContent className="bg-white">
-        <SelectItem value="house">House</SelectItem>
-        <SelectItem value="apartment">Apartment</SelectItem>
-      </SelectContent>
-    </Select>
-      
-    <FormMessage />
-  </FormItem>
-)}
-/>
-<div className="grid grid-cols-12 gap-4">
+          <FormField
+            control={form.control}
+            name="property_type"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Property Type</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select property type" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent className="bg-white">
+                    <SelectItem value="house">House</SelectItem>
+                    <SelectItem value="apartment">Apartment</SelectItem>
+                  </SelectContent>
+                </Select>
 
-<div className="col-span-4">
-  
-<FormField
-control={form.control}
-name="address"
-render={({ field }) => (
-  <FormItem>
-    <FormLabel>Street Address</FormLabel>
-    <FormControl>
-      <Input 
-      placeholder="123 Main St"
-      
-      type=""
-      {...field} />
-    </FormControl>
-    
-    <FormMessage />
-  </FormItem>
-)}
-/>
-</div>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="grid grid-cols-12 gap-4">
+            <div className="col-span-4">
+              <FormField
+                control={form.control}
+                name="address"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Street Address</FormLabel>
+                    <FormControl>
+                      <Input placeholder="123 Main St" type="" {...field} />
+                    </FormControl>
 
-</div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
 
-<div className="grid grid-cols-12 gap-4">
+          <div className="grid grid-cols-12 gap-4">
+            <div className="col-span-6">
+              <FormField
+                control={form.control}
+                name="city"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>City</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Melbourne" type="text" {...field} />
+                    </FormControl>
 
-<div className="col-span-6">
-  
-<FormField
-control={form.control}
-name="city"
-render={({ field }) => (
-  <FormItem>
-    <FormLabel>City</FormLabel>
-    <FormControl>
-      <Input 
-      placeholder="Melbourne"
-      
-      type="text"
-      {...field} />
-    </FormControl>
-    
-    <FormMessage />
-  </FormItem>
-)}
-/>
-</div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
 
-<div className="col-span-6">
-  
-<FormField
-control={form.control}
-name="state"
-render={({ field }) => (
-  <FormItem>
-    <FormLabel>State</FormLabel>
-    <FormControl>
-      <Input 
-      placeholder="VIC"
-      type="text"
-      {...field} />
-    </FormControl>
-    
-    <FormMessage />
-  </FormItem>
-)}
-/>
-</div>
+            <div className="col-span-6">
+              <FormField
+                control={form.control}
+                name="state"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>State</FormLabel>
+                    <FormControl>
+                      <Input placeholder="VIC" type="text" {...field} />
+                    </FormControl>
 
-</div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
 
+          <div className="grid grid-cols-12 gap-4">
+            <div className="col-span-6">
+              <FormField
+                control={form.control}
+                name="postal_code"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Postal Code</FormLabel>
+                    <FormControl>
+                      <Input placeholder="3000" type="" {...field} />
+                    </FormControl>
 
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
 
-<div className="grid grid-cols-12 gap-4">
+            <div className="col-span-6">
+              <FormField
+                control={form.control}
+                name="apartment_number"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Unit/Apt #</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Apt 4B" type="" {...field} />
+                    </FormControl>
 
-<div className="col-span-6">
-  
-<FormField
-control={form.control}
-name="postal_code"
-render={({ field }) => (
-  <FormItem>
-    <FormLabel>Postal Code</FormLabel>
-    <FormControl>
-      <Input 
-      placeholder="3000"
-      
-      type=""
-      {...field} />
-    </FormControl>
-    
-    <FormMessage />
-  </FormItem>
-)}
-/>
-</div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
 
-<div className="col-span-6">
-  
-<FormField
-control={form.control}
-name="apartment_number"
-render={({ field }) => (
-  <FormItem>
-    <FormLabel>Unit/Apt #</FormLabel>
-    <FormControl>
-      <Input 
-      placeholder="Apt 4B"
-      
-      type=""
-      {...field} />
-    </FormControl>
-    
-    <FormMessage />
-  </FormItem>
-)}
-/>
-</div>
+          <FormField
+            control={form.control}
+            name="monthly_rent"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Monthly Rent</FormLabel>
+                <FormControl>
+                  <Input placeholder="2500" type="number" {...field} />
+                </FormControl>
 
-</div>
-</form>
-</Form>
-</div>
-  )
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="bond"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Bond / Security Deposit</FormLabel>
+                <FormControl>
+                  <Input placeholder="2500" type="number" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
 }
