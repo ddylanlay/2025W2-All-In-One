45173ee8b3e47a6ad0597a442aff6066ed9a754c
nalydh <dylanhoang9@gmail.com>
2025-04-23 18:36:19 +1000

    86cym3x36: Reduce vertical space between fields

--- app/client/ui-modules/property-form-agent/components/BasicInformation.tsx
+++ app/client/ui-modules/property-form-agent/components/BasicInformation.tsx
@@ -42,7 +42,7 @@ export default function BasicInformation() {
       <h1 className="text-xl mt-5 mb-2 space-y-8 max-w-3xl mx-auto font-semibold">Basic Information</h1>
       <h3 className="text-sm  max-w-3xl mx-auto text-[#71717A]">Enter the basic details of the property</h3>
       <Form {...form}>
-        <form className="space-y-8 max-w-3xl mx-auto py-7">
+        <form className="space-y-4 max-w-3xl mx-auto py-7">
           <FormField
             control={form.control}
             name="landlord"
