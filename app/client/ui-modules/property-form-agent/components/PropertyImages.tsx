6fe753ebf5dd95b6efb3552ef424e79a6812e1af
nalydh <dylanhoang9@gmail.com>
2025-04-27 17:24:51 +1000

    86cym3x36: Implemented Property Images section of the form as a component

---
+++ app/client/ui-modules/property-form-agent/components/PropertyImages.tsx
@@ -0,0 +1,84 @@
+"use client"
+import React, { useState } from "react";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "../../theming/components/shadcn/Form";
+import {
+  CloudUpload,
+  Paperclip
+} from "lucide-react";
+import * as z from "zod";
+import {
+  FileUploader,
+  FileUploaderContent,
+  FileUploaderItem,
+  FileInput,
+} from "../../theming/components/shadcn/FileUpload";
+import { formSchema } from "../PropertyForm";
+import { UseFormReturn } from "react-hook-form";
+
+type FormSchemaType = z.infer<typeof formSchema>
+
+export default function PropertyImages({ form }: { form: UseFormReturn<FormSchemaType>}) {
+
+  const [files, setFiles] = useState < File[] | null > (null);
+
+  const dropZoneConfig = {
+    maxFiles: 20,
+    maxSize: 1024 * 1024 * 10,
+    multiple: true,
+  };
+
+  return (
+    <div className="border border-[#E4E4E7] w-full p-7 rounded-md">
+      <h1 className="text-xl mb-1 space-y-8 max-w-3xl mx-auto font-semibold">Property Images</h1>
+      <h3 className="text-sm mb-2 max-w-3xl mx-auto text-[#71717A]">Upload high-quality images of the property</h3>
+            <FormField
+              control={form.control}
+              name="images"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <FileUploader
+                      value={files}
+                      onValueChange={setFiles}
+                      dropzoneOptions={dropZoneConfig}
+                      className="relative bg-background rounded-lg p-2"
+                    >
+                      <FileInput
+                        id="fileInput"
+                        className="outline-dashed outline-1 outline-[#E4E4E7]"
+                      >
+                        <div className="flex items-center justify-center flex-col p-8 w-full ">
+                          <CloudUpload className='text-gray-500 w-10 h-10' />
+                          <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
+                            <span className="font-semibold">Click to upload</span>
+                            &nbsp;or drag and drop
+                          </p>
+                          <p className="text-xs text-gray-500 dark:text-gray-400">
+                          Upload up to 20 images (JPEG, PNG, WebP) up to 10MB each
+                          </p>
+                        </div>
+                      </FileInput>
+                      <FileUploaderContent>
+                        {files &&
+                          files.length > 0 &&
+                          files.map((file, i) => (
+                            <FileUploaderItem key={i} index={i}>
+                              <Paperclip className="h-4 w-4 stroke-current" />
+                              <span>{file.name}</span>
+                            </FileUploaderItem>
+                          ))}
+                      </FileUploaderContent>
+                    </FileUploader>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            </div>
+  )
+}
