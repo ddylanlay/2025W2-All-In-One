641b33ca478ac9921eca9f888f482e40c66d3c63
Your Name <you@example.com>
2025-04-28 22:01:39 +1000

    86cyn7391 - file restructure to have each role dashboard in one module

--- app/client/store.ts
+++ app/client/store.ts
@@ -1,6 +1,6 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import { homePageSlice } from "./ui-modules/home-example/state/reducers/home-page-slice";
-import { agentDashboardSlice } from "./ui-modules/agent-dashboard/state/agent-dashboard-slice";
+import { agentDashboardSlice } from "./ui-modules/role-dashboard/agent-dashboard/state/agent-dashboard-slice";
 import { guestLandingPageSlice } from "./ui-modules/guest-landing-page/state/reducers/guest-landing-page-slice";
 export const store = configureStore({
   reducer: {
