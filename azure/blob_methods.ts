87f3564c67367b428ae114c236edb7797007aa18
Tristan Nguyen <tngu0294@student.monash.edu>
2025-04-30 15:42:52 +1000

    86cym3xbe: Add File Upload functions for Azure

--- azure/blob_methods.ts
+++ azure/blob_methods.ts
@@ -1,22 +1,16 @@
 import { Meteor } from 'meteor/meteor';
-import { getContainerClient } from '../app/server/azure/blobStorageService';
+import { getContainerClient, uploadFile, uploadFiles, UploadResults } from '../app/server/azure/blobStorageService';
 console.log("✅ blobs.list method file loaded");
 
 
 Meteor.methods({
   // Method to upload blob
-  async 'blobs.upload'(data: any) {
+  async 'blobs.uploadFile'(blob: Blob,blobName: string) {
     try {
       console.log("Uploading...")
-      // Your blob upload logic here using containerClient
-      const content = "Hello";
-      const contentBuffer = Buffer.from(content, "utf-8");
       const containerClient = await getContainerClient();
-      const blockBlobClient = containerClient.getBlockBlobClient("hello.txt");
-      await blockBlobClient.upload(contentBuffer,contentBuffer.length);
-      console.log("Upload Complete")
-      // ... rest of your upload logic
-      return { success: true };
+      const uploadResult = await uploadFile(containerClient, blobName, blob);
+        return uploadResult; 
     } catch (error) {
         if (error instanceof Error) {
           throw new Meteor.Error('blob-upload-failed', error.message);
@@ -26,33 +20,30 @@ Meteor.methods({
       }
     },
 
-  // Method to download blob
-  async 'blobs.download'(blobName: string) {
-    try {
+  async 'blobs.uploadFiles'(blobs: Blob[], blobNamePrefix: string) {
+    try{
       const containerClient = await getContainerClient();
-      const blobClient = containerClient.getBlobClient(blobName);
-      // ... your download logic
-      return { success: true };
+      const uploadResults: UploadResults = await uploadFiles(containerClient, blobs, blobNamePrefix);
+      return uploadResults
     } catch (error) {
         if (error instanceof Error) {
           throw new Meteor.Error('blob-upload-failed', error.message);
         } else {
           throw new Meteor.Error('blob-upload-failed', 'Unknown error occurred');
         }
-      }      
+      }
+
   },
 
   // Method to list blobs
   async 'blobs.list'() {
     try {
-      console.log("BLOBS!!!!!")
       const containerClient = await getContainerClient();
       const blobs = [];
       for await (const blob of containerClient.listBlobsFlat()) {
         blobs.push(blob.name);
       }
-      return ["WE DID IT"]
-      // return blobs;
+      return blobs;
     } catch (error) {
         if (error instanceof Error) {
           throw new Meteor.Error('blob-upload-failed', error.message);
