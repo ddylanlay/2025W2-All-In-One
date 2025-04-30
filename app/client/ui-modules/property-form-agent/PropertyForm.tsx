5dcfc9951cd12de745eec37ace4ddfc26cb4ad08
nalydh <dylanhoang9@gmail.com>
2025-04-30 13:56:33 +1000

    86cym3x36: Hide formSchema logic away from Property Form

--- app/client/ui-modules/property-form-agent/PropertyForm.tsx
+++ app/client/ui-modules/property-form-agent/PropertyForm.tsx
@@ -11,44 +11,11 @@ import FormPropertyImages from "./components/FormPropertyImages";
 import FormListingOptions from "./components/FormListingOptions";
 import { Button } from "../theming/components/shadcn/Button";
 import { ArrowLeftIcon } from "lucide-react";
-import { PageHeading } from "./components/PageHeading";
 import { FormDefaultValue } from "./components/FormDefaultValues";
-
-export const formSchema = z.object({
-  landlord: z.string().min(1, { message: "Please assign a landlord" }),
-  property_type: z
-    .string()
-    .min(1, { message: "Please select a property type" }),
-  address: z.string().min(1, { message: "Please fill out field" }),
-  city: z.string().min(1, { message: "Please fill out field" }),
-  state: z.string().min(1, { message: "Please fill out field" }),
-  postal_code: z
-    .string()
-    .length(4, { message: "Postal code must be 4 digits long" }),
-  apartment_number: z.string().optional(),
-  monthly_rent: z.coerce
-    .number()
-    .min(1, { message: "Monthly rent must be greater than 0" }),
-  bond: z.coerce.number().min(1, { message: "Please fill out field" }),
-  bedroom_number: z.coerce
-    .number()
-    .min(1, { message: "Please fill out field" }),
-  bathroom_number: z.coerce
-    .number()
-    .min(1, { message: "Please fill out field" }),
-  space: z.coerce.number().min(1, { message: "Please fill out field" }),
-  description: z.string().min(1, { message: "Please fill out field" }),
-  amenities: z.string().min(1, { message: "Please fill out field" }),
-  images: z
-    .array(z.instanceof(File))
-    .min(1, { message: "At least one image is required" }),
-  available_dates: z.coerce.date(),
-  lease_term: z.string().min(1, { message: "Lease term is required" }),
-  show_contact_boolean: z.boolean().optional(),
-});
+import { formSchema, FormSchemaType } from "./components/FormSchema";
 
 export function PropertyForm(): React.JSX.Element {
-  const form = useForm<z.infer<typeof formSchema>>({
+  const form = useForm<FormSchemaType>({
     resolver: zodResolver(formSchema),
     defaultValues: FormDefaultValue,
   });
@@ -71,11 +38,12 @@ export function PropertyForm(): React.JSX.Element {
           <ArrowLeftIcon className="scale-75" />{" "}
           <span className="text-md">Back to Properties</span>
         </button>
-        <PageHeading
-          title="Property Listing"
-          subtitle="Create a new rental property listing for a landlord"
-          className=""
-        />
+        <div>
+          <h1 className="text-2xl font-bold mb-1">Property Listing</h1>
+          <h3 className="text-sm text-[#71717A]">
+            Create a new rental property listing for a landlord
+          </h3>
+        </div>
       </div>
 
       <div className="max-w-3xl mx-auto px-6 py-10 rounded-md">
