9447bffc8c02831f6225d2b0a89db12629bdb42a
tonyl15 <tonyly0009@gmail.com>
2025-05-05 14:36:37 +1000

    86cym42q5: Refactor code to follow JSX format

--- app/client/ui-modules/agent-properties/AgentProperties.tsx
+++ app/client/ui-modules/agent-properties/AgentProperties.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import EditDraftListingModal from "./components/EditDraftListingModal";
 
 export function AgentProperties() {
-  const { isOpen, toggle, ModalUI } = EditDraftListingModal();
-
   return (
     <div>
       <h1>Agent properties</h1>
-      <button onClick={toggle}>Click this to edit a property listing draft</button>
-      {ModalUI}
+      <EditDraftListingModal></EditDraftListingModal>
     </div>
   );
 }
