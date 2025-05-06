d8ab87deb1e478ed7fc15489eaca46c1390dd610
Tristan Nguyen <tngu0294@student.monash.edu>
2025-05-06 20:31:06 +1000

    86cym3xbe: Remove Unused File BlobFunctions.ts

--- azure/BlobFunctions.ts
+++
@@ -1,5 +0,0 @@
-export async function viewBlobs(): Promise<string[]> {
-    console.log("Calling blobs.list");
-    const blobs: string[] = await Meteor.callAsync("blobs.list", null)
-    return blobs;
-}
