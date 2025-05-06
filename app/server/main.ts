5b1fdbec5b49e83bbb993e20435f99b5cf1a190a
babb0004 <babb0004@student.monash.edu>
2025-05-06 15:41:40 +1000

    86cyrzuyd removed unnecessary code in server main file

--- app/server/main.ts
+++ app/server/main.ts
@@ -1,17 +1,4 @@
 import { Meteor } from "meteor/meteor";
-import { Accounts } from "meteor/accounts-base";
 import "./methods/accounts/user.register";
 
-// todo: initial set up to then be changed later
-const SEED_USERNAME = 'meteorite';
-const SEED_PASSWORD = 'password';
-
-Meteor.startup(async () => {
-  if (!(await Accounts.findUserByUsername(SEED_USERNAME))) {
-    await Accounts.createUser({
-      username: SEED_USERNAME,
-      password: SEED_PASSWORD,
-      email: "example.email.com"
-    });
-  }
-});
+Meteor.startup(async () => {});
