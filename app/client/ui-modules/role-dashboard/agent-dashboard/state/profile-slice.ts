24428bb4a42bb94ba5f89f7bf19e9ae0cda44c26
Higgo8888 <thig0003@student.monash.edu>
2025-05-09 14:29:50 +1000

    Created the profile-slice.ts class to utilise the states of being able to edit the profile information. Added this into the the store.ts too.

---
+++ app/client/ui-modules/role-dashboard/agent-dashboard/state/profile-slice.ts
@@ -0,0 +1,89 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../../../../store";
+
+interface ProfileData {
+  firstName: string;
+  lastName: string;
+  dob: string;
+  occupation: string;
+  email: string;
+  phone: string;
+  emergencyContact: string;
+  employer: string;
+  workAddress: string;
+  workPhone: string;
+}
+
+interface ProfileState {
+  data: ProfileData;
+  isEditing: boolean;
+  isLoading: boolean;
+  error: string | null;
+}
+
+const initialState: ProfileState = {
+  isEditing: false,
+  isLoading: false,
+  error: null,
+  data: {
+    firstName: "Tom",
+    lastName: "Macauley",
+    dob: "25/09/2003",
+    occupation: "Student",
+    email: "thomas123mac@gmail.com",
+    phone: "0437 559 777",
+    emergencyContact: "Thomas Higgins (Bestest Friend) - (000)",
+    employer: "Fit 3170",
+    workAddress: "Learning Jungle",
+    workPhone: "0437 559 777"
+  }
+};
+
+export const profileSlice = createSlice({
+  name: "profile",
+  initialState,
+  reducers: {
+    setEditing: (state, action: PayloadAction<boolean>) => {
+      state.isEditing = action.payload;
+    },
+    updateField: (state, action: PayloadAction<{ field: keyof ProfileData; value: string }>) => {
+      state.data[action.payload.field] = action.payload.value;
+    },
+    saveProfile: (state) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    saveProfileSuccess: (state) => {
+      state.isLoading = false;
+      state.isEditing = false;
+    },
+    saveProfileFailure: (state, action: PayloadAction<string>) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    resetProfile: (state) => {
+      // You might want to reset to initial state or fetch fresh data
+      state.isEditing = false;
+      state.error = null;
+    }
+  }
+});
+
+// Action creators
+export const { 
+  setEditing, 
+  updateField, 
+  saveProfile, 
+  saveProfileSuccess, 
+  saveProfileFailure,
+  resetProfile 
+} = profileSlice.actions;
+
+// Selectors
+export const selectProfile = (state: RootState) => state.profile;
+export const selectProfileData = (state: RootState) => state.profile.data;
+export const selectIsEditing = (state: RootState) => state.profile.isEditing;
+export const selectIsLoading = (state: RootState) => state.profile.isLoading;
+export const selectProfileError = (state: RootState) => state.profile.error;
+
+export default profileSlice.reducer;
