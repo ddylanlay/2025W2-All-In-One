9a11d6c3cb14d833e502e3557192a88f782a40a4
Shen-SLJ <shen.jng1@gmail.com>
2025-05-11 19:11:11 +1000

    86cybzj4m: Add new error type to describe invalid data

--- app/server/methods/property/property-methods.ts
+++ app/server/methods/property/property-methods.ts
@@ -4,6 +4,7 @@ import {
   PropertyFeatureCollection,
   PropertyStatusCollection,
 } from "/app/server/database/property/PropertyCollections";
+import { InvalidDataError } from "/app/server/errors/InvalidDataError";
 import { PropertyDTO } from "/app/server/methods/property/models/PropertyDTO";
 import { MeteorMethodIdentifier } from "/app/shared/meteor-method-identifier";
 
@@ -14,13 +15,13 @@ const propertyGetMethod = {
     const fetchedPropertyDocument = await PropertyCollection.findOneAsync(id);
 
     if (!fetchedPropertyDocument) {
-      throw new Meteor.Error(`Property with ${id} not found`);
+      throw new Meteor.Error("not-found", `Property with ${id} not found`);
     }
 
     const propertyDTO = mapPropertyDocumentToPropertyDTO(
       fetchedPropertyDocument
     ).catch((error) => {
-      throw new Meteor.Error(error);
+      throw new Meteor.Error("invalid-data", error.message);
     });
 
     return propertyDTO;
@@ -30,37 +31,41 @@ const propertyGetMethod = {
 async function mapPropertyDocumentToPropertyDTO(
   property: Property
 ): Promise<PropertyDTO> {
-  return new Promise(async (resolve, reject) => {
-    const propertyStatus = await PropertyStatusCollection.findOneAsync(
-      property.property_status_id
+  const propertyStatusDocument = await PropertyStatusCollection.findOneAsync(
+    property.property_status_id
+  );
+  const propertyFeaturesDocuments = await PropertyFeatureCollection.find({
+    _id: { $in: property.property_feature_ids },
+  }).fetchAsync();
+
+  if (!propertyStatusDocument) {
+    throw new InvalidDataError(
+      `Property status for Property id ${property._id} not found.`
     );
-    const propertyFeatures = await PropertyFeatureCollection.find({
-      _id: { $in: property.property_feature_ids },
-    }).fetchAsync();
+  }
+  if (
+    propertyFeaturesDocuments.length !== property.property_feature_ids.length
+  ) {
+    throw new InvalidDataError(
+      `Missing property features for Property id ${property._id}.`
+    );
+  }
 
-    if (!propertyStatus) {
-      reject(`Property status for Property id ${property._id} not found`);
-    }
-    if (propertyFeatures.length !== property.property_feature_ids.length) {
-      reject(`Missing property features for Property id ${property._id}.`);
-    }
-
-    resolve({
-      propertyId: property._id,
-      streetnumber: property.streetnumber,
-      streetname: property.streetname,
-      suburb: property.suburb,
-      province: property.province,
-      postcode: property.postcode,
-      propertyStatus: property.property_status_id, // will need to obtain status from the collection
-      description: property.description,
-      bathrooms: property.bathrooms,
-      bedrooms: property.bedrooms,
-      features: property.property_feature_ids, // will need to obtain fetures from the collection
-      type: property.type,
-      area: property.area,
-    });
-  });
+  return {
+    propertyId: property._id,
+    streetnumber: property.streetnumber,
+    streetname: property.streetname,
+    suburb: property.suburb,
+    province: property.province,
+    postcode: property.postcode,
+    propertyStatus: propertyStatusDocument.name, // will need to obtain status from the collection
+    description: property.description,
+    bathrooms: property.bathrooms,
+    bedrooms: property.bedrooms,
+    features: propertyFeaturesDocuments.map((doc) => doc.name), // will need to obtain fetures from the collection
+    type: property.type,
+    area: property.area,
+  };
 }
 
 Meteor.methods({
