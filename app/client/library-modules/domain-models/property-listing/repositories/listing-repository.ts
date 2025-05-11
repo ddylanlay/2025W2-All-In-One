b0278730e45997e7b3c13a4c4fc643ebd02528ab
Shen-SLJ <shen.jng1@gmail.com>
2025-05-11 23:51:27 +1000

    86cybzj4m: Create use case for getting property and listing data in one

--- app/client/library-modules/domain-models/property-listing/repositories/listing-repository.ts
+++ app/client/library-modules/domain-models/property-listing/repositories/listing-repository.ts
@@ -1,8 +1,8 @@
-import { apiGetListingForProperty } from "/app/client/library-modules/apis/property-listings/listing-api";
+import { apiGetListingForProperty } from "../../../apis/property-listing/listing-api";
 import { Listing } from "/app/client/library-modules/domain-models/property-listing/Listing";
-import { mapApiListingToListing } from "/app/client/library-modules/domain-models/property-listing/repositories/listing-mapper";
+import { mapApiListingToListing } from "./mappers/listing-mapper";
 
-export async function getListingForProperty(propertyId: string): Promise<Listing> {
+export async function getListingDataForProperty(propertyId: string): Promise<Listing> {
   const apiListing = await apiGetListingForProperty(propertyId);
   const mappedListing = mapApiListingToListing(apiListing);
 
--- app/client/library-modules/domain-models/property/repositories/property-repository.ts
+++ app/client/library-modules/domain-models/property/repositories/property-repository.ts
@@ -1,6 +1,6 @@
 import { apiGetPropertyById } from "/app/client/library-modules/apis/property/property-api";
 import { Property } from "/app/client/library-modules/domain-models/property/Property";
-import { mapApiPropertyToProperty } from "/app/client/library-modules/domain-models/property/repositories/property-mapper";
+import { mapApiPropertyToProperty } from "./mappers/property-mapper";
 
 export async function getPropertyById(id: string): Promise<Property> {
   const apiProperty = await apiGetPropertyById(id);
