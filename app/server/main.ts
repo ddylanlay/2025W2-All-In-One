caec075698eccb880cd0930d41c7c535436ab19d
Tristan Nguyen <tngu0294@student.monash.edu>
2025-05-12 20:52:41 +1000

    86cym3xbe: remove duplicate dependency, remove helper functiotn

--- app/server/main.ts
+++ app/server/main.ts
@@ -1,5 +1,4 @@
 import { Meteor } from "meteor/meteor";
 import "./methods/accounts/user.register";
 import "./methods/azure/blob_methods";
-import "./settings-to-env";
 Meteor.startup(async () => {});
