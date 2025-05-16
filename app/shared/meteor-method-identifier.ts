672a1d1f03fdf986775b0b14414493ead4f997c7
babb0004 <babb0004@student.monash.edu>
2025-05-16 16:37:06 +1000

    86cyrzv5y : created user methods files for all the db crud methods

--- app/shared/meteor-method-identifier.ts
+++ app/shared/meteor-method-identifier.ts
@@ -4,5 +4,6 @@ export enum MeteorMethodIdentifier {
   TASK_GET_ALL = 'tasks.getAll',
   PROPERTY_GET = 'properties.getOne',
   LISTING_GET_FOR_PROPERTY = 'listings.getForProperty',
-  USER_REGISTER = 'user.register'
+  USER_REGISTER = 'user.register',
+  USER_INSERT = 'users.insert'
 }
