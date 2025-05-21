e7ad9fec8deff6269102dc682482ff8e3add1230
Tom Macauley <thomas123mac@gmail.com>
2025-05-21 11:44:44 +1000

    86cyrzxwu: Created "get" methods for the profile data collection

--- app/server/methods/user/profile-data-methods.ts
+++ app/server/methods/user/profile-data-methods.ts
@@ -1,46 +1,76 @@
 import { ProfileDataDocument } from "../../database/user/models/role-models/ProfileDataDocument";
+import { ProfileCollection } from "../../database/user/user-collections";
+import { InvalidDataError } from "../../errors/InvalidDataError";
+import { meteorWrappedInvalidDataError } from "../../utils/error-utils";
 import { ApiProfileData } from "/app/shared/api-models/user/api-roles/ApiProfileData";
 import { MeteorMethodIdentifier } from "/app/shared/meteor-method-identifier";
 
+/// client method
 const getProfileDataMethod = {
-  [MeteorMethodIdentifier.PROFILE_GET]: async (
-    userId: string
-  ): Promise<ApiProfileData> => {
-    // TODO : implement
-    return undefined;
-  },
+    [MeteorMethodIdentifier.PROFILE_GET]: async (
+        userId: string
+    ): Promise<ApiProfileData> => {
+        // TODO : implement
+        const profileDataDocument = await getProfileDataDocumentById(userId);
+
+        if (!profileDataDocument) {
+            throw meteorWrappedInvalidDataError(
+                new InvalidDataError(
+                    `Data for user with ID ${userId} not found.`
+                )
+            );
+        }
+
+        try {
+            const profileDataDTO = await mapProfileDataDocumentToDTO(
+                profileDataDocument
+            );
+            return profileDataDTO;
+        } catch (error: any) {
+            throw meteorWrappedInvalidDataError(
+                error instanceof Error ? error : new Error("Unknown error")
+            );
+        }
+    },
 };
+
+// Data access from backend
+async function getProfileDataDocumentById(
+    id: string
+): Promise<ProfileDataDocument | undefined> {
+    return await ProfileCollection.findOneAsync({ _id: id });
+}
 
 const updateProfileDataMethod = {
-  [MeteorMethodIdentifier.PROFILE_EDIT]: async (
-    userId: string
-  ): Promise<ApiProfileData> => {
-    // TODO : implement
-    return undefined;
-  },
+    [MeteorMethodIdentifier.PROFILE_EDIT]: async (
+        userId: string
+    ): Promise<ApiProfileData> => {
+        // TODO : implement
+        return undefined;
+    },
 };
 
-function mapProfileDataDocumentToDTO(
-  profile: ProfileDataDocument
+async function mapProfileDataDocumentToDTO(
+    profile: ProfileDataDocument
 ): Promise<ApiProfileData> {
-  return {
-    userAccountId: profile._id,
-    firstName: profile.firstName,
-    lastName: profile.lastName,
-    email: profile.email,
-    dob: profile.email,
-    occupation: profile.occupation,
-    phone: profile.phone,
-    emergencyContact: profile.emergencyContact,
-    employer: profile.employer,
-    workAddress: profile.workAddress,
-    workPhone: profile.workPhone,
-    profilePicture: profile.profilePicture,
-    carMake: profile.carMake,
-    carModel: profile.carModel,
-    carYear: profile.carYear,
-    carPlate: profile.carPlate,
-  };
+    return {
+        userAccountId: profile._id,
+        firstName: profile.firstName,
+        lastName: profile.lastName,
+        email: profile.email,
+        dob: profile.email,
+        occupation: profile.occupation,
+        phone: profile.phone,
+        emergencyContact: profile.emergencyContact,
+        employer: profile.employer,
+        workAddress: profile.workAddress,
+        workPhone: profile.workPhone,
+        profilePicture: profile.profilePicture,
+        carMake: profile.carMake,
+        carModel: profile.carModel,
+        carYear: profile.carYear,
+        carPlate: profile.carPlate,
+    };
 }
 
 Meteor.methods({ ...updateProfileDataMethod, ...getProfileDataMethod });
