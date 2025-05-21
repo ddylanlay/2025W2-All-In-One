aa31db39ad5c1a9af2b6fbf7084f3453736c712e
Tom Macauley <thomas123mac@gmail.com>
2025-05-21 14:40:48 +1000

    86cyrzxwu: Created dummy data to appear on the database

--- app/server/database/user/user-collections.ts
+++ app/server/database/user/user-collections.ts
@@ -6,12 +6,12 @@ import { LandlordDocument } from "./models/role-models/LandlordDocument";
 import { ProfileDataDocument } from "./models/role-models/ProfileDataDocument";
 
 export const UserAccountCollection: Mongo.Collection<UserAccountDocument> =
-  new Mongo.Collection("user_accounts");
+    new Mongo.Collection("user_accounts");
 export const AgentCollection: Mongo.Collection<AgentDocument> =
-  new Mongo.Collection("agents");
+    new Mongo.Collection("agents");
 export const TenantCollection: Mongo.Collection<TenantDocument> =
-  new Mongo.Collection("tenants");
+    new Mongo.Collection("tenants");
 export const LandlordCollection: Mongo.Collection<LandlordDocument> =
-  new Mongo.Collection("landlords");
+    new Mongo.Collection("landlords");
 export const ProfileCollection: Mongo.Collection<ProfileDataDocument> =
-  new Mongo.Collection("profileData");
+    new Mongo.Collection("profile_data");
