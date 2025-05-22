b378ffa4c3d320feebde81e3e84b6f52a72d82da
nalydh <dylanhoang9@gmail.com>
2025-05-22 13:42:49 +1000

    86cyyhbdn: Add domain wrapper for getting property status ID

--- app/client/library-modules/domain-models/property/repositories/property-repository.ts
+++ app/client/library-modules/domain-models/property/repositories/property-repository.ts
@@ -1,10 +1,16 @@
 import { apiGetPropertyById } from "/app/client/library-modules/apis/property/property-api";
 import { Property } from "/app/client/library-modules/domain-models/property/Property";
 import { mapApiPropertyToProperty } from "./mappers/property-mapper";
+import { PropertyStatus } from "/app/shared/api-models/property/PropertyStatus";
+import { apiGetPropertyStatusId } from "/app/client/library-modules/apis/property/property-api";
 
 export async function getPropertyById(id: string): Promise<Property> {
   const apiProperty = await apiGetPropertyById(id);
   const mappedProperty = mapApiPropertyToProperty(apiProperty);
 
   return mappedProperty;
+}
+
+export async function getPropertyStatusId(name: PropertyStatus): Promise<string> {
+  return await apiGetPropertyStatusId(name);
 }
