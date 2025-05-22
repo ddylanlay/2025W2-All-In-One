f456c5fe8d463463c852706d662ba9c025cd2d39
Your Name <you@example.com>
2025-05-22 21:28:01 +1000

    86cz0ug47 - display count of properties for particular agent

--- app/client/ui-modules/role-dashboard/agent-dashboard/components/DashboardCard.tsx
+++ app/client/ui-modules/role-dashboard/agent-dashboard/components/DashboardCard.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Progress } from '../../components/ProgressBar';
 import { CardWidget } from '../../components/CardWidget';
+import { Meteor } from 'meteor/meteor';
+import { MeteorMethodIdentifier } from '/app/shared/meteor-method-identifier';
+import { useAppSelector } from '/app/client/store';
 
 export function DashboardCards() {
+  const [pendingTasksCount, setPendingTasksCount] = useState<number>(0);
+  const [propertyCount, setPropertyCount] = useState<number>(0);
+  const currentUser = useAppSelector((state) => state.currentUser.currentUser);
+  useEffect(() => {
+    const getPropertyCount = async () => {
+      if (currentUser && 'agentId' in currentUser && currentUser.agentId) {
+        try {
+          const count = await Meteor.callAsync(MeteorMethodIdentifier.PROPERTY_GET_COUNT, currentUser.agentId);
+          setPropertyCount(count);
+        } catch (error) {
+          console.error('Error fetching property count:', error);
+        }
+      }
+    };
+    getPropertyCount();
+  }, [currentUser]);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-6">
       <CardWidget
         title="Total Properties"
-        value="12"
+        value= {propertyCount.toString()}
         subtitle="+2 from last month"
       />
       <CardWidget
