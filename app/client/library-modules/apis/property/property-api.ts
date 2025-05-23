96a10d6b55bae57f74774735fcc9b431b74c4e20
tlyy0017 <tlyy0017@student.monash.edu>
2025-05-23 14:12:41 +1000

    86cyrhc1n: Add wrapper API function to update property

--- app/client/library-modules/apis/property/property-api.ts
+++ app/client/library-modules/apis/property/property-api.ts
@@ -2,6 +2,7 @@ import { PropertyStatus } from "/app/shared/api-models/property/PropertyStatus";
 import { ApiProperty } from "/app/shared/api-models/property/ApiProperty";
 import { MeteorMethodIdentifier } from "/app/shared/meteor-method-identifier";
 import { PropertyInsertData } from "/app/shared/api-models/property/PropertyInsertData";
+import { Property } from "../../domain-models/property/Property";
 
 export async function apiGetPropertyById(id: string): Promise<ApiProperty> {
   const fetchedProperty = await Meteor.callAsync(MeteorMethodIdentifier.PROPERTY_GET, id);
@@ -17,4 +18,6 @@ export async function apiInsertProperty(property: PropertyInsertData): Promise<s
   return await Meteor.callAsync(MeteorMethodIdentifier.PROPERTY_INSERT, property);
 }
 
-
+export async function apiUpdateProperty(updatedProperty: Property): Promise<string> {
+  return await Meteor.callAsync(MeteorMethodIdentifier.PROPERTY_UPDATE, updatedProperty);
+}
