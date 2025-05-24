7eefb6992d840ade4515e5dbfbd014c8c8413cf8
Tristan Nguyen <tngu0294@student.monash.edu>
2025-05-24 23:23:22 +1000

    86cz0u7zt: Add Property Price Insert Method

---
+++ app/server/methods/property-price/property-price-method.ts
@@ -0,0 +1,14 @@
+import { PropertyPriceCollection } from "../../database/property/property-collections";
+import { MeteorMethodIdentifier } from "/app/shared/meteor-method-identifier";
+import { PropertyPriceInsertData } from "/app/shared/api-models/property-price/PropertyPriceInsertData";
+const insertPropertyPriceForProperty = { [MeteorMethodIdentifier.PROPERTY_PRICE_INSERT]: async (
+propertyPriceData: PropertyPriceInsertData
+): Promise<string> => {
+    const propertyPrice = await PropertyPriceCollection.insertAsync(propertyPriceData);
+    return propertyPrice;
+}
+}
+
+
+Meteor.methods({
+    ...insertPropertyPriceForProperty})
