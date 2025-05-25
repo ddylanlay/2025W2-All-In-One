0e63191dfc8f94ae3d6f2e81e177ea3683927505
Tristan Nguyen <tngu0294@student.monash.edu>
2025-05-25 11:13:49 +1000

    86cz0u7zt: Linting

--- app/server/methods/property-price/property-price-method.ts
+++ app/server/methods/property-price/property-price-method.ts
@@ -1,14 +1,17 @@
 import { PropertyPriceCollection } from "../../database/property/property-collections";
 import { MeteorMethodIdentifier } from "/app/shared/meteor-method-identifier";
 import { PropertyPriceInsertData } from "/app/shared/api-models/property-price/PropertyPriceInsertData";
-const insertPropertyPriceForProperty = { [MeteorMethodIdentifier.PROPERTY_PRICE_INSERT]: async (
-propertyPriceData: PropertyPriceInsertData
-): Promise<string> => {
-    const propertyPrice = await PropertyPriceCollection.insertAsync(propertyPriceData);
+const insertPropertyPriceForProperty = {
+  [MeteorMethodIdentifier.PROPERTY_PRICE_INSERT]: async (
+    propertyPriceData: PropertyPriceInsertData
+  ): Promise<string> => {
+    const propertyPrice = await PropertyPriceCollection.insertAsync(
+      propertyPriceData
+    );
     return propertyPrice;
-}
-}
-
+  },
+};
 
 Meteor.methods({
-    ...insertPropertyPriceForProperty})
+  ...insertPropertyPriceForProperty,
+});
