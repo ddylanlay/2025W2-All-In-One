d5d728d9dac29443eee76852385b4de3184ae20f
shannonWallis <swal0059@student.monash.edu>
2025-05-26 15:44:39 +1000

    86cz20dk0: Add the task date option, property option to the form for adding a task

--- app/client/ui-modules/role-dashboard/agent-dashboard/components/TaskFormInformation.tsx
+++ app/client/ui-modules/role-dashboard/agent-dashboard/components/TaskFormInformation.tsx
@@ -16,16 +16,27 @@ import {
 } from "../../../theming-shadcn/Select";
 import { Input } from "../../../theming-shadcn/Input";
 import { UseFormReturn } from "react-hook-form";
-import { FormSchemaType } from "../../../property-form-agent/components/FormSchema";
+import { TaskFormSchemaType } from "./TaskFormSchema";
 import { FormHeading } from "../../../property-form-agent/components/FormHeading";
 import { Landlord } from "/app/client/library-modules/domain-models/user/Landlord";
-
+import { Property } from "/app/client/library-modules/domain-models/property/Property";
+import { Calendar } from "../../../theming-shadcn/Calendar";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { Switch } from "../../theming-shadcn/Switch";
+import { Button } from "../../theming-shadcn/Button";
+import {
+  RadioGroup,
+  RadioGroupItem,
+} from "../../../theming-shadcn/RadioGroup";
+import { Popover, PopoverContent, PopoverTrigger } from "../../../theming-shadcn/Popover";
 export default function FormBasicInformation({
   form,
   landlords,
+  properties,
 }: {
-  form: UseFormReturn<FormSchemaType>;
+  form: UseFormReturn<TaskFormSchemaType>;
   landlords: Landlord[];
+  properties: Property[];
 }) {
   return (
     <div className="border border-(--divider-color) w-full p-7 rounded-md mb-3">
@@ -39,17 +50,20 @@ export default function FormBasicInformation({
         render={({ field }) => (
           <FormItem>
             <FormLabel>Assign To </FormLabel>
-            <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <Select
+              onValueChange={(val) => field.onChange(val)}
+              defaultValue={field.value !== undefined ? String(field.value) : undefined}
+            >
               <FormControl>
                 <SelectTrigger>
-                  <SelectValue placeholder="Select a tenant, landlord or yourself" />
+                  <SelectValue placeholder="Assign a tenant, landlord or yourself" />
                 </SelectTrigger>
               </FormControl>
               <SelectContent className="bg-white">
                 {landlords.map((landlord) => (
                   <SelectItem
-                    key={landlord.landlordId}
-                    value={landlord.landlordId}
+                    key={String(landlord.landlordId)}
+                    value={String(landlord.landlordId)}
                   >
                     {landlord.firstName} {landlord.lastName}
                   </SelectItem>
@@ -67,19 +81,22 @@ export default function FormBasicInformation({
         render={({ field }) => (
           <FormItem>
             <FormLabel>Property</FormLabel>
-            <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <Select
+              onValueChange={field.onChange}
+              defaultValue={field.value !== undefined ? String(field.value) : undefined}
+            >
               <FormControl>
                 <SelectTrigger>
-                  <SelectValue placeholder="Select Property" />
+                  <SelectValue placeholder="Select a Property" />
                 </SelectTrigger>
               </FormControl>
               <SelectContent className="bg-white">
-                {landlords.map((landlord) => (
+                {properties.map((property) => (
                   <SelectItem
-                    key={landlord.landlordId}
-                    value={landlord.landlordId}
+                    key={property.propertyId}
+                    value={property.propertyId}
                   >
-                    {landlord.firstName} {landlord.lastName}
+                    {property.streetnumber} {property.streetname} {property.postcode}
                   </SelectItem>
                 ))}
               </SelectContent>
@@ -91,42 +108,71 @@ export default function FormBasicInformation({
 
       <FormField
         control={form.control}
-        name="property_type"
+        name="task_type"
         render={({ field }) => (
           <FormItem className="py-2">
-            <FormLabel>Property Type</FormLabel>
+            <FormLabel>Task Type</FormLabel>
             <Select onValueChange={field.onChange} defaultValue={field.value}>
               <FormControl>
                 <SelectTrigger>
-                  <SelectValue placeholder="Select property type" />
+                  <SelectValue placeholder="Select task type" />
                 </SelectTrigger>
               </FormControl>
               <SelectContent className="bg-white">
-                <SelectItem value="house">House</SelectItem>
-                <SelectItem value="apartment">Apartment</SelectItem>
+                <SelectItem value="house">Inspection</SelectItem>
+                <SelectItem value="apartment">Maintenance</SelectItem>
+                <SelectItem value="apartment">Admin</SelectItem>
+                <SelectItem value="apartment">Document signing</SelectItem>
+                <SelectItem value="apartment">Cleaning</SelectItem>
+                <SelectItem value="apartment">Online Meeting</SelectItem>
+                <SelectItem value="apartment">In Person Meeting</SelectItem>
               </SelectContent>
             </Select>
-
             <FormMessage />
           </FormItem>
         )}
       />
       <div className="grid grid-cols-12 gap-4">
         <div className="col-span-4">
-          <FormField
-            control={form.control}
-            name="address"
-            render={({ field }) => (
-              <FormItem className="py-2">
-                <FormLabel>Street Address</FormLabel>
+        <FormField
+        control={form.control}
+        name="task_duedate"
+        render={({ field }) => (
+          <FormItem className="flex flex-col py-2">
+            <FormLabel>Task Due</FormLabel>
+            <Popover>
+              <PopoverTrigger asChild>
                 <FormControl>
-                  <Input placeholder="123 Main St" type="" {...field} />
+                  <Button
+                    variant={"outline"}
+                    className={`w-[240px] pl-3 text-left font-normal border-[--divider-color] ${!field.value ? "text-muted-foreground" : ""}`}
+                  >
+                    {field.value ? (
+                      format(field.value, "PPP")
+                    ) : (
+                      <span>Pick a date</span>
+                    )}
+                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                  </Button>
                 </FormControl>
+              </PopoverTrigger>
+              <PopoverContent
+                className="w-auto p-0 border-(--divider-color) bg-white"
+                align="start"
+              >
+                <Calendar
+                  mode="single"
+                  selected={field.value}
+                  onSelect={field.onChange}
+                  initialFocus
+                />
+              </PopoverContent>
+            </Popover>
 
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+            <FormMessage />
+          </FormItem>
+        )}
+      />
         </div>
       </div>
 
