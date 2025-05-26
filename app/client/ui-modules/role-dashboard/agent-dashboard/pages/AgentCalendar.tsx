d0762518263c7800148a463084c2a1c64e6860f4
babb0004 <babb0004@student.monash.edu>
2025-05-26 10:21:29 +1000

    86cyvdrq0 : merge main to the branch

--- app/client/ui-modules/role-dashboard/agent-dashboard/pages/AgentCalendar.tsx
+++ app/client/ui-modules/role-dashboard/agent-dashboard/pages/AgentCalendar.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../../../store";
-import { setTasks, selectTasks } from "../state/agent-dashboard-slice";
 import {
   fetchAgentTasks,
   selectTasks,
