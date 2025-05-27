6826c56e3f8776740e86893981f2612d4903fde1
Tristan Nguyen <tngu0294@student.monash.edu>
2025-05-27 21:35:02 +1000

    86cz0u7zt: Handle Mapping Error with FormSchema

--- app/client/library-modules/domain-models/property/repositories/mappers/property-mapper.ts
+++ app/client/library-modules/domain-models/property/repositories/mappers/property-mapper.ts
@@ -1,6 +1,9 @@
 import { ApiProperty } from "/app/shared/api-models/property/ApiProperty";
 import { Property } from "/app/client/library-modules/domain-models/property/Property";
-import { PropertyFormData, PropertyInsertData } from "/app/shared/api-models/property/PropertyInsertData";
+import { PropertyInsertData } from "/app/shared/api-models/property/PropertyInsertData";
+import { FormSchemaType } from "/app/client/ui-modules/property-form-agent/components/FormSchema";
+import { getPropertyStatusId } from "../property-repository";
+import { PropertyStatus } from "/app/shared/api-models/property/PropertyStatus";
 
 export function mapApiPropertyToProperty(property: ApiProperty): Property {
   return {
@@ -27,30 +30,30 @@ export function mapApiPropertyToProperty(property: ApiProperty): Property {
 }
 
 
-export function mapPropertyFormDataToInsertData(formData: PropertyFormData) {
+export async function mapFormSchemaToPropertyInsertData(form: FormSchemaType) {
   const propertyInsertData: PropertyInsertData = {
-    streetnumber: formData.streetnumber,
-    streetname: formData.streetname,
-    suburb: formData.suburb,
-    province: formData.province,
-    postcode: formData.postcode,
-    property_status_id: formData.property_status_id,
-    description: formData.description,
-    summary_description: formData.summary_description,
-    bathrooms: formData.bathrooms,
-    bedrooms: formData.bedrooms,
-    parking: formData.parking,
-    property_feature_ids: formData.property_feature_ids,
-    type: formData.type,
-    area: formData.area,
-    agent_id: formData.agent_id,
-    landlord_id: formData.landlord_id,
-    tenant_id: formData.tenant_id,
+    streetnumber: form.address_number,
+    streetname: form.address,
+    suburb: form.suburb,
+    province: form.state, // adjust if you have a separate province field
+    postcode: form.postal_code,
+    property_status_id: await getPropertyStatusId(PropertyStatus.VACANT), // fill as needed
+    description: form.description,
+    summary_description: form.description.slice(0,60), // fill as needed
+    bathrooms: form.bathroom_number,
+    bedrooms: form.bedroom_number,
+    parking: 0, // fill as needed
+    property_feature_ids: form.property_feature_ids,
+    type: form.property_type,
+    area: form.space,
+    agent_id: "", // fill as needed
+    landlord_id: form.landlord,
+    tenant_id: "", // fill as needed
   };
 
   return {
     propertyInsertData,
-    monthly_rent: formData.monthly_rent,
-    images: formData.images,
+    monthly_rent: form.monthly_rent,
+    images: form.images,
   };
 }
