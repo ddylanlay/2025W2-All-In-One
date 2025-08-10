6892dd27e077f33ecfb7800b5e73c7388e5e05c3
Dylan Lay <laydylan13@gmail.com>
2025-08-11 00:28:32 +1000

    86czvm6b6-removed prop inspection

--- app/client/ui-modules/guest-landing-page/state/reducers/guest-landing-page-slice.ts
+++ app/client/ui-modules/guest-landing-page/state/reducers/guest-landing-page-slice.ts
@@ -40,23 +40,12 @@ export const guestLandingPageSlice = createSlice({
       })
       .addCase(fetchPropertiesAndListings.fulfilled, (state, action) => {
         state.isLoading = false;
-<<<<<<< HEAD
-        // Convert Date objects to strings to make them serializable
-        state.properties = action.payload.map(property => ({
-          ...property,
-          inspections: property.inspections.map(inspection => ({
-            start_time: inspection.start_time instanceof Date ? inspection.start_time.toISOString() : inspection.start_time,
-            end_time: inspection.end_time instanceof Date ? inspection.end_time.toISOString() : inspection.end_time,
-          }))
-        }));
-=======
         const skip = action.meta.arg.skip;
         if (skip === 0) {
           state.properties = action.payload;
         } else {
           state.properties = [...state.properties, ...action.payload];
         }
->>>>>>> 8cf3b93070b51985548ba5af1530fa226da84255
       })
       .addCase(fetchPropertiesAndListings.rejected, (state, action) => {
         state.isLoading = false;
