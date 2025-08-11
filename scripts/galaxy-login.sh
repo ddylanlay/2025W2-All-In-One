9edb4fd098cbd270e93d64b6733a17e2781258e8
Tristan Nguyen <tngu0294@student.monash.edu>
2025-08-11 16:28:01 +1000

    86czy7g09: Settime out and log

--- scripts/galaxy-login.sh
+++ scripts/galaxy-login.sh
@@ -1,5 +1,12 @@
 #!/home/linuxbrew/.linuxbrew/bin/expect -f
 
+# Enable logging to console and file
+log_user 1
+log_file login.log
+
+# Increase timeout to 30 seconds
+set timeout 30
+
 if {$argc != 2} {
   puts "The script takes exactly two arguments, username and password in sequence."
   exit
