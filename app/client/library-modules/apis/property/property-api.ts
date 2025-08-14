23498a6dd7a0a3010fc20eb2bd9a498600685e06
Tom Macauley <thomas123mac@gmail.com>
2025-08-14 14:59:50 +1000

    86czx3kwr: Fixed merging issues with property ai, seperated the navigating to a utility function rather than having same logic in both search results page and guest landing page

--- app/client/library-modules/apis/property/property-api.ts
+++ app/client/library-modules/apis/property/property-api.ts
@@ -6,7 +6,6 @@ import { PropertyUpdateData } from "/app/shared/api-models/property/PropertyUpda
 import { Meteor } from "meteor/meteor";
 
 export async function apiGetPropertyById(id: string): Promise<ApiProperty> {
-<<<<<<< HEAD
     try {
         const fetchedProperty = await Meteor.callAsync(
             MeteorMethodIdentifier.PROPERTY_GET,
@@ -66,8 +65,16 @@ export async function apiGetPropertyByAgentId(
     agentId: string
 ): Promise<ApiProperty[]> {
     return await Meteor.callAsync(
-        MeteorMethodIdentifier.PROPERTY_GET_LIST,
+        MeteorMethodIdentifier.PROPERTY_GET_ALL_BY_AGENT_ID,
         agentId
+    );
+}
+export async function apiGetAllPropertiesByLandlordId(
+    landlordId: string
+): Promise<ApiProperty[]> {
+    return await Meteor.callAsync(
+        MeteorMethodIdentifier.PROPERTY_GET_ALL_BY_LANDLORD_ID,
+        landlordId
     );
 }
 
@@ -78,76 +85,4 @@ export async function apiSearchPropertyByQuery(
         MeteorMethodIdentifier.PROPERTY_SEARCH,
         query
     );
-=======
-  try {
-    const fetchedProperty = await Meteor.callAsync(
-      MeteorMethodIdentifier.PROPERTY_GET,
-      id
-    );
-    return fetchedProperty;
-  } catch (error) {
-    console.error(`Error fetching property with ID ${id}:`, error);
-    throw error;
-  }
-}
-
-export async function apiGetPropertyStatusId(
-  name: PropertyStatus
-): Promise<string> {
-  return await Meteor.callAsync(
-    MeteorMethodIdentifier.PROPERTY_STATUS_GET,
-    name
-  );
-}
-
-export async function apiInsertProperty(
-  property: PropertyInsertData
-): Promise<string> {
-  return await Meteor.callAsync(
-    MeteorMethodIdentifier.PROPERTY_INSERT,
-    property
-  );
-}
-
-export async function apiUpdatePropertyData(
-  updatedProperty: PropertyUpdateData
-): Promise<string> {
-  return await Meteor.callAsync(
-    MeteorMethodIdentifier.PROPERTY_DATA_UPDATE,
-    updatedProperty
-  );
-}
-
-export async function apiGetAllProperties(): Promise<ApiProperty[]> {
-  const fetchedProperties: ApiProperty[] = await Meteor.callAsync(
-    MeteorMethodIdentifier.PROPERTY_GET_ALL
-  );
-  return fetchedProperties;
-}
-
-export async function apiGetPropertyByTenantId(
-  tenantId: string
-): Promise<ApiProperty> {
-  return await Meteor.callAsync(
-    MeteorMethodIdentifier.PROPERTY_GET_BY_TENANT_ID,
-    tenantId
-  );
-}
-
-export async function apiGetPropertyByAgentId(
-  agentId: string
-): Promise<ApiProperty[]> {
-  return await Meteor.callAsync(
-    MeteorMethodIdentifier.PROPERTY_GET_ALL_BY_AGENT_ID,
-    agentId
-  );
-}
-export async function apiGetAllPropertiesByLandlordId(
-  landlordId: string
-): Promise<ApiProperty[]> {
-  return await Meteor.callAsync(
-    MeteorMethodIdentifier.PROPERTY_GET_ALL_BY_LANDLORD_ID,
-    landlordId
-  );
->>>>>>> b29a32a901ce8506088cd32fc93186eac647cb23
 }
