b752ffcc36a680285eb8b019c560a765c5b3b60a
Tom Macauley <thomas123mac@gmail.com>
2025-08-28 18:42:49 +1000

    86d02bzq2: Created database, api and client methods for retrieving and uploading a lease agreement

---
+++ app/client/library-modules/domain-models/user-documents/LeaseAgreement.ts
@@ -0,0 +1,8 @@
+export type LeaseAgreementDocument = {
+	_id: string; // Unique identifier
+	propertyId: string; // ID of the property
+	agentId: string; // ID of the agent managing the lease
+	uploadedDate: Date; // Date when the document was uploaded
+	validUntil: Date; // Expiry date of the lease agreement
+	documentUrl: string; // URL to access the document
+};
