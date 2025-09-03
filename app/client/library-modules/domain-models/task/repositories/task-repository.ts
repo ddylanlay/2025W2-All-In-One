cb77e2aeaa0a9be9fb193d550487065c23377caf
Shannon Wallis <swal0059@student.monash.edu>
2025-09-03 10:42:59 +1000

    86d05e3c0 - Use the Repo no the api in the calendar when creating tasks

--- app/client/library-modules/domain-models/task/repositories/task-repository.ts
+++ app/client/library-modules/domain-models/task/repositories/task-repository.ts
@@ -1,10 +1,27 @@
 import { apiGetTaskById } from "../../../apis/task/task-api";
 import { Task } from "/app/client/library-modules/domain-models/task/Task";
 import { mapApiTasktoTask } from "./mappers/task-mapper";
+import { apiCreateTaskForLandlord } from "../../../apis/task/task-api";
+import { TaskData } from "../../../../ui-modules/role-dashboard/agent-dashboard/components/TaskFormSchema"
 
 export async function getTaskById(id: string): Promise<Task> {
   const apiTask = await apiGetTaskById(id);
   const mappedTask = mapApiTasktoTask(apiTask);
 
   return mappedTask;
+}
+
+export async function createTaskForLandlord(
+  taskData: TaskData,
+  userId: string
+): Promise<string> {
+  const apiData = {
+    ...taskData,
+    userId, // add userId
+    dueDate: new Date(taskData.dueDate), // ensure date is Date type
+    propertyId: taskData.propertyId || "", // default if missing
+  };
+
+  const createdTaskId = await apiCreateTaskForLandlord(apiData);
+  return createdTaskId;
 }
