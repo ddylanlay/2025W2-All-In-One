d1a19f8772fe1f60155d3dd492b24b90d09a24c1
rpar0034 <rpar0034@student.monash.edu>
2025-09-04 14:27:29 +1000

    86czzhcn2: Log in History server side

--- app/client/library-modules/apis/user/user-account-api.ts
+++ app/client/library-modules/apis/user/user-account-api.ts
@@ -1,7 +1,19 @@
 import { Meteor } from "meteor/meteor";
 import { MeteorMethodIdentifier } from "/app/shared/meteor-method-identifier";
 import { ApiUserAccount } from "/app/shared/api-models/user/ApiUserAccount";
+import { ApiLoginHistoryPage } from "../../../../shared/api-models/user/ApiLoginHistory";
 
 export async function apiGetUserAccount(userId: string): Promise<ApiUserAccount> {
   return await Meteor.callAsync(MeteorMethodIdentifier.USER_ACCOUNT_GET, userId);
+}
+
+export async function apiGetLoginHistory(
+  userId: string,
+  page: number,
+  pageSize: number
+): Promise<ApiLoginHistoryPage> {
+  return await Meteor.callAsync(
+    MeteorMethodIdentifier.LOGIN_HISTORY_GET_FOR_USER,
+    { userId, page, pageSize }
+  );
 }
